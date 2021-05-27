6dc55b8dd28922076ba10809b47f97abb34357de
felixalt12 <84901214+felixalt12@users.noreply.github.com>
2021-05-27 10:24:54 +0200

    Update Enteros.java

--- Enteros.java
+++ Enteros.java
@@ -1,69 +1,69 @@
 package enteros;
 public class Enteros {
 public static int r_factorial(int n) {
-		int resultado;
+		int resultado1;
 
 		if (n == 0) {
-			resultado = 1;
+			resultado1 = 1;
 		} else {
-			resultado = n * r_factorial(n - 1);
+			resultado1 = n * r_factorial(n - 1);
 		}
-		return resultado;
+		return resultado1;
 }
 public static int factorial(int n) {
-		int resultado;
+		int resultado1;
 
-		resultado = 1;
+		resultado1 = 1;
 		for (int i = 2; i <= n; i++) {
-			resultado = resultado * i;
+			resultado1 = resultado1 * i;
 		}
 		return resultado;
 }
 public static String alreves(String cadena) {
-		String resultado = "";
+		String resultado1 = "";
 
 		for (int i = cadena.length() - 1; i >= 0; i--) {
-			resultado = resultado + cadena.charAt(i);
+			resultado1 = resultado1 + cadena.charAt(i);
 		}
-		return resultado;
+		return resultado1;
 }
 public static boolean capicua(int n) {
-		boolean resultado;
+		boolean resultado1;
 		String cadena;
 
 		cadena = Integer.toString(n);
 		if (cadena.equals(alreves(cadena))) {
-			resultado = true;
+			resultado1 = true;
 		} else {
 			resultado = false;
 		}
 
-		return resultado;
+		return resultado1;
 }
 public static boolean divisible(int multiplo, int divisor) {
-		boolean resultado;
+		boolean resultado1;
 
 		if (multiplo % divisor == 0) {
-			resultado = true;
+			resultado1 = true;
 		} else {
-			resultado = false;
+			resultado1 = false;
 		}
 
-		return resultado;
+		return resultado1;
 }
 public static boolean multiplo(int mult, int divisor) {
-		boolean resultado;
+		boolean resultado1;
 
 		try {
 			if (mult % divisor == 0) {
-				resultado = true;
+				resultado1 = true;
 			} else {
-				resultado = false;
+				resultado1 = false;
 			}
 		} catch (ArithmeticException e) {
-			resultado = false;
+			resultado1= false;
 		}
-		return resultado;
+		return resultado1
 }
 public static boolean esPrimo(int n) {
 		boolean primo;
@@ -84,42 +84,42 @@ public static boolean esPrimo(int n) {
 		return primo;
 }
 public static int n_primos(int n) {
-		int resultado = 0;
+		int resultado1 = 0;
 
 		for (int i = 2; i <= n; i++) {
 			if (esPrimo(i)) {
-				resultado++;
+				resultado1 ++;
 			}
 		}
-		return resultado;
+		return resultado1;
 }
 public static int[] primosHasta(int n) {
-		int[] resultado;
+		int[] resultado1;
 
 		resultado = new int[n_primos(n)];
 		int cont = 0;
 		for (int i = 2; i <= n; i++) {
 			if (esPrimo(i)) {
-				resultado[cont] = i;
+				resultado1[cont] = i;
 				cont++;
 			}
 		}
 
-		return resultado;
+		return resultado1;
 }
 public static int[] primerosPrimos(int n) {
-		int[] resultado = new int[(n < 0) ? 0 : n];
+		int[] resultado1 = new int[(n < 0) ? 0 : n];
 
 		int cont = 0;
 		int i = 2;
 		while (cont < n) {
 			if (esPrimo(i)) {
-				resultado[cont] = i;
+				resultado1[cont] = i;
 				cont++;
 			}
 			i++;
 		}
 
-		return resultado;
+		return resultado1;
 	}
 }
